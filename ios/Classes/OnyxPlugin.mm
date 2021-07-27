70b1a020ad51c9235ecb6ecf6a369f56668b66a6
Tony Allison <tonyallison@advsysunlimited.com>
2021-07-27 13:34:02 -0500

    re-added missing code

--- ios/Classes/OnyxPlugin.mm
+++ ios/Classes/OnyxPlugin.mm
@@ -192,5 +192,24 @@ handles the onyx error callback.
     return returnArray;
 }
 
+- (UIViewController *)topViewController{
+   return [self topViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+ }
+
+ - (UIViewController *)topViewController:(UIViewController *)rootViewController
+ {
+   if ([rootViewController isKindOfClass:[UINavigationController class]]) {
+     UINavigationController *navigationController = (UINavigationController *)rootViewController;
+     return [self topViewController:[navigationController.viewControllers lastObject]];
+   }
+   if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+     UITabBarController *tabController = (UITabBarController *)rootViewController;
+     return [self topViewController:tabController.selectedViewController];
+   }
+   if (rootViewController.presentedViewController) {
+     return [self topViewController:rootViewController];
+   }
+   return rootViewController;
+ }
 
 @end
